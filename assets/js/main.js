f532020d8085fd4f788843d9e4286351e4932ec0
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-09-24 10:01:22 +0100

    Tailwind setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-function o(e){let t=0;const n=c=>{t=c,e.innerHTML=`count is ${t}`};e.addEventListener("click",()=>n(t+1)),n(0)}o(document.querySelector("#counter"));
+function o(t){let n=0;const e=c=>{n=c,t.innerHTML=`count is ${n}`};t.addEventListener("click",()=>e(n+1)),e(0)}o(document.querySelector("#counter"));
--- vite.config.cjs
+++ vite.config.cjs
@@ -6,7 +6,10 @@ module.exports = {
       input: 'assets/src/main.ts',
       output: {
         entryFileNames: `js/[name].js`,
-        chunkFileNames: `js/[name].js`
+        chunkFileNames: `js/[name].js`,
+        // Works for CSS, will be a problem if we start
+        // using images. Would export to, e.g., `assets/jpg/`
+        assetFileNames: `[ext]/[name].[ext]`,
       }
     }
   }
