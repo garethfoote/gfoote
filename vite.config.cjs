70553056851d00f38b59202d88ade21638b126a8
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-09-24 11:07:25 +0100

    CSS, sass and tailwind setup

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   build: {
-    outDir: 'assets',
+    outDir: '.',
     emptyOutDir: false,
     rollupOptions: {
       input: 'assets/src/main.ts',
       output: {
-        entryFileNames: `js/[name].js`,
-        chunkFileNames: `js/[name].js`,
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
         // Works for CSS, will be a problem if we start
         // using images. Would export to, e.g., `assets/jpg/`
-        assetFileNames: `[ext]/[name].[ext]`,
+        assetFileNames: `_sass/tailwind.css`,
       }
     }
   }
