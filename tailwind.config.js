455f8427d2c672261d72c5d9a2e819b8f45d61ed
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-10-18 22:29:45 +0100

    A lot of minor style modifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,17 @@ module.exports = {
       'aquamarine': {
         light: '#B8FEE7',
         DEFAULT: '#1CFCB2',
-        dark: '#17E29F'
+        dark: '#40484C',
+        darkest: '#0CAD78'
       },
       'purple': '#7e5bef', // possibly unused
       'offwhite': '#FCFCFC',
       'black': '#0B0C0C',
       'grey':  {
-        light: '#C3C3C3',
+        lightest: '#C3C3C3',
+        light: '#8E9498',
         DEFAULT: '#505A5F',
-        dark: '#505A5F'
+        dark: '#303639',
       },
     },
     fontFamily: {
@@ -26,9 +28,10 @@ module.exports = {
     },
     fontSize: {
       sm: ['0.875rem', '1.25rem'],
-      base: ['1rem', '1.625rem'],
-      lg: ['1.25rem', '2rem'],
-      xl: ['1.562rem', '2rem'],
+      base: ['1rem', '1.625rem'], // 16px, 26px
+      md: ['1.125rem', '1.75rem'], // 18px, 26px
+      lg: ['1.25rem', '2rem'], // 20px, 32px
+      xl: ['1.562rem', '2rem'], // 25px, 32px 
       '2xl': ['1.938rem', '2.25rem'],
       '3xl': ['2.438rem', '2.75rem'],
       '4xl': ['3.062rem', '3.5rem'],
