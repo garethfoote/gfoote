205c54cf9e45df550de9eba28cf8b3f59fbf3747
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-10-24 18:08:51 +0100

    Changes to TS dir structure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import "./index.css"
-import { draw } from './squiggle.ts'
+import { draw } from './ts/squiggle.ts'
 
 const canvasContainer = document.querySelector(".squiggle") as HTMLElement;
 const canvas = document.getElementById("squiggle") as HTMLCanvasElement;
 
-window.addEventListener("load", ()=> {
+window.addEventListener("load", () => {
   canvasContainer.classList.remove("squiggle--hidden");
   draw(canvas, 150, 150);
 })
