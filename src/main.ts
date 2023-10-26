b90c1ea13dfd8ac0fbf5166ee433624a5de805be
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-10-26 10:24:04 +0100

    Tabs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./index.css"
 import { drawSquiggle } from './ts/squiggle.ts'
+import { Tabs } from './ts/tabs.ts'
 
 const canvasContainer = document.querySelector(".squiggle") as HTMLElement;
 const canvas = document.getElementById("squiggle") as HTMLCanvasElement;
@@ -8,3 +9,12 @@ window.addEventListener("load", () => {
   canvasContainer.classList.remove("squiggle--hidden");
   drawSquiggle(canvas, 150, 150);
 })
+
+const tabsEl: HTMLElement | null = document.querySelector(
+  "[data-module=tabs]"
+);
+if(tabsEl){
+  const tabs: Tabs = new Tabs(tabsEl);
+  console.log(tabs)
+  tabs.initialise();
+}
---
+++ src/ts/tabs.ts
@@ -0,0 +1,89 @@
+export class Tabs {
+  private tabs: HTMLElement[];
+  private panels: HTMLElement[];
+  private currIndex: number = 0; 
+
+  constructor(root: HTMLElement) {
+    this.tabs = Array.from(root.querySelectorAll("[role=tablist] [role=tab]"));
+    this.panels = []
+  }
+
+  initialise: () => void = () => {
+    this.tabs.forEach((tab:HTMLElement) => {
+      const panelName : string = tab.getAttribute('aria-controls')!;
+      const panel = document.getElementById(panelName)!;
+      
+      tab.tabIndex = -1
+      tab.setAttribute('aria-selected', 'false');
+      this.panels.push(panel);
+
+      tab.addEventListener('keydown', this.onKeydown);
+      tab.addEventListener('click', this.onClick);  
+    })
+
+    this.setSelectedTab(this.tabs[0], false);
+  };
+
+  setSelectedTab: (tab: HTMLElement, setFocus?:boolean) => void = (
+    tab, 
+    setFocus=true
+  ) => {
+    const panel:HTMLElement|undefined = this.panels.find((p)=> tab.getAttribute("aria-controls") === p.id);
+
+    if(!panel) return;
+
+    tab.setAttribute('aria-selected', 'true');
+    tab.tabIndex = 0;
+    tab.parentElement?.classList.add('tabs__list-item--selected');
+    panel.classList.remove('tabs__panel--hidden');
+
+    this.tabs.forEach((t, i)=>{
+      if(tab != t){
+        t.setAttribute('aria-selected', 'false');
+        t.parentElement?.classList.remove('tabs__list-item--selected');
+        t.tabIndex = -1;
+        this.panels[i].classList.add('tabs__panel--hidden');
+      }
+    })
+
+    if(setFocus == true){
+      tab.focus();
+    }
+
+    // this.currIndex = newIndex
+    console.log(this.currIndex)
+
+  }
+
+  onClick: (e: MouseEvent) => void = (e) => {
+    console.log(e.currentTarget)
+    this.setSelectedTab(e.currentTarget as HTMLElement)
+
+  }
+
+  onKeydown: (e: KeyboardEvent) => void = (e) => {
+    console.log(e.key);
+
+    switch (e.key) {
+      case 'ArrowLeft':
+        // this.setSelectedToPreviousTab(tgt);
+        break;
+
+      case 'ArrowRight':
+        // this.setSelectedToNextTab(tgt);
+        break;
+
+      case 'Home':
+        // this.setSelectedTab(this.firstTab);
+        break;
+
+      case 'End':
+        // this.setSelectedTab(this.lastTab);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+  
