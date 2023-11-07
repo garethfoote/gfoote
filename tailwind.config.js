5c78ad47c6a245584f213ede77c364ac05f11737
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-11-07 13:48:04 +0000

    XS textx

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,7 @@ module.exports = {
       serif: ['EB Garamond', 'serif'],
     },
     fontSize: {
+      xs: ['0.6875rem', '1.25rem'], // 13px
       sm: ['0.875rem', '1.25rem'], // 13px
       base: ['1rem', '1.625rem'], // 16px, 26px
       md: ['1.125rem', '1.75rem'], // 18px, 28px
