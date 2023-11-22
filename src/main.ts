e8cb4ff0c36e66ebbf82c36904d79a31c3d6b8e7
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-11-22 18:28:45 +0000

    Hero image in content

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,13 @@ if(tabsEl){
   console.log(tabs)
   tabs.initialise();
 }
+
+// If first paragraph in content contains an image we
+// assume this is a hero type image and remove the 
+// max-width constraint from the <p>
+const firstP = document.querySelector(".content p");
+
+if(firstP?.firstElementChild?.tagName == "IMG"){
+  firstP.classList.add("hero");
+  console.log('firstP', firstP)
+}
